b88533de9053347cfb3cfc889ec59cab54cf9641
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-04-27 21:12:11 +0800

    Demonstrate schematics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { TosComponent } from './tos/tos.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FormComponent } from './form/form.component';
 
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
+  { path: 'nav', component: NavigationComponent },
+  { path: 'form', component: FormComponent },
   { path: 'terms', component: TosComponent },
   { path: 'privacy', component: PrivacyComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { FormComponent } from './form/form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TosComponent,
     PrivacyComponent,
-    HomepageComponent
+    HomepageComponent,
+    NavigationComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +40,16 @@ import { MatDividerModule } from '@angular/material/divider';
     BrowserAnimationsModule,
     MatButtonModule,
     MatDividerModule,
-    MatIconModule
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
