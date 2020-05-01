3bc02967a73c95b74a92f83d09b0e1b649eabad7
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-05-01 12:45:37 +0800

    Snackbar demo

--- src/app/snackbardemo/snackbardemo.component.ts
+++ src/app/snackbardemo/snackbardemo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snackbardemo',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SnackbardemoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  click1() {
+    const snackBarRef = this.snackBar.open('Evil was unleashed', 'Undo', {duration: 3000});
+
+    snackBarRef.afterDismissed().subscribe(() => {
+      console.log('Dismissed!');
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      console.log('Action Triggered');
+    });
   }
 
 }
