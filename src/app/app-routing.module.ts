c1b3245ecb571ba458687707e3cb6f815e2c8068
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-05-03 14:14:54 +0800

    Add placeholder component for the other pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {PrivacyPageComponent} from './privacy-page/privacy-page.component';
+import {TermsPageComponent} from './terms-page/terms-page.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'privacy', component: PrivacyPageComponent},
+  {path: 'terms', component: TermsPageComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,17 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PrivacyPageComponent } from './privacy-page/privacy-page.component';
+import { TermsPageComponent } from './terms-page/terms-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutPageComponent,
+    PrivacyPageComponent,
+    TermsPageComponent
   ],
   imports: [
     BrowserModule,
